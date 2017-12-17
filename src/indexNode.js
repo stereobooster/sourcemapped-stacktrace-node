29351b6edd7d1331cb1ff104b364a0501a55645d
stereobooster <stereobooster@gmail.com>
2017-12-17 10:14:14 +0100

    Store options

--- src/indexNode.js
+++ src/indexNode.js
@@ -3,6 +3,7 @@ const createNodeResolver = require("./createNodeResolver.js").default;
 
 exports.createNodeResolver = createNodeResolver;
 exports.default = (stack, opts) => {
-  opts.resolver = opts.resolver || createNodeResolver({});
-  return mapStackTrace(stack, opts);
+  const { store, ...other } = opts;
+  other.resolver = other.resolver || createNodeResolver({ store });
+  return mapStackTrace(stack, other);
 };
